fddf36088c8b27fb6cce236afe7bdfe018b5c4ab
Vain <marinreynaezequiel@gmail.com>
2022-10-24 17:11:14 -0300

    Add files via upload

--- Labo4/proyecto4.c
+++ Labo4/proyecto4.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <assert.h>
 #include <limits.h>
+#include <math.h>
+#include <stdlib.h>
 
   int pedirEntero (void){
      int x;
@@ -106,31 +108,130 @@ int main (){
 } */
 //ejercicio 2
 /*int main (){
-    int x, y, xaux;
+    int x, y, xaux, yaux;
     x= pedirEntero();
     y= pedirEntero ();
     xaux=x;
+    yaux=y;
+    
+    assert(xaux == x && yaux == y);
 
     x= x + 1;
-    y= xaux + y;
+    y= xaux + yaux;
+    
+    assert(x == xaux + 1 && y == xaux + yaux);
 
     printf ("El valor de x es %d\nEl valor de y es %d\n",x,y);
     return 0;
 }*/
 
-int main (){
-    int x, y, z, xaux, yaux;
+/*int main (){
+    int x, y, z, xaux, yaux, zaux;
     x= pedirEntero();
     y= pedirEntero();
     z= pedirEntero();
     xaux=x;
     yaux=y;
+    zaux=z;
 
-    x= y;
-    y= xaux + y + z;
+    assert(xaux == x && yaux == y && zaux == z);
+    
+    x = yaux;
+    y= xaux + yaux + zaux;
     z= yaux + xaux;
     
+    assert(x == yaux && y == xaux + yaux + zaux && z == yaux + xaux);
+    
     printf ("El valor de x es %d\nEl valor de y es %d\nEl valor de z es %d\n",x,y,z);
+
     return 0;
+}*/
+
+//Ejercicio3//
+
+/*int sumaHasta (int N){
+  
+  int i, resul;
+
+  i = 0;
+  resul = 0;
+
+  while ( i < N){
+
+      resul = resul + i;
+      i = i + 1;
+
+  }
+   
+   return resul;
+   
 }
 
+int main (){    
+    
+    int x;
+    x = pedirEntero();
+
+    assert( x >= 0);
+
+    printf("%d\n",sumaHasta(x));
+
+    return 0;
+}*/
+
+//Ejercicio4//
+
+/*struct div_t {
+    int cociente;
+    int resto;
+    };
+
+struct div_t division(int x, int y){
+    
+    struct div_t res ;
+    
+    res.resto = (x % y);
+    res.cociente = (x/y);
+
+	printf(
+		"El cociente es %d y el resto es %d\n", 
+		res.cociente, 
+		res.resto
+		);
+
+	return res;
+ }
+
+int main()
+{
+	int x, y; 
+    x = pedirEntero();
+    y = pedirEntero();
+    
+    assert(y > 0 && x >= 0);
+
+    division(x,y);
+
+	return 0;
+}*/
+/* numero= divisor * cociente + resto  */
+
+void pedirArreglo(int a[], int n_max){
+
+    a[] =  
+    i = 0;
+
+    while (n >= i){
+        a[i]
+        i = i + 1
+
+    } 
+}
+void imprimeArreglo(int a[], int n_max){
+    printf(pedirArreglo)
+}
+int main()
+{
+    /* code */
+    return 0;
+}
